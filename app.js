30896085e7124226120ac2afbac7910c8ebcb8eb
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-01 17:40:12 -0500

    i like to have fun

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hey')
+console.log('motherfucking binary search, yeah!')
+
+const display = document.querySelector('.display')
+const header = document.createElement('h1')
+header.innerText = 'Mother Fucking Binary Search, YEAH!!!!!!!!!!!!!!!!'
+header.style.display = 'flex'
+header.style.justifyContent = 'center'
+
+display.appendChild(header)
